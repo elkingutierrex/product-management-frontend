5d62ffed7f22abc5ec3c3b6e84af2355e153162e
elkingutierrex <elkin.gutierrex@gmail.com>
2026-03-09 21:54:04 -0500

    feat(header)

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,27 @@
+import { Package2 } from 'lucide-react'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container mx-auto flex h-14 max-w-screen-2xl items-center px-4 md:px-8">
+        <Link to="/" className="mr-6 flex items-center space-x-2">
+          <div className="bg-primary/10 p-1.5 rounded-md">
+            <Package2 className="h-5 w-5 text-primary" />
+          </div>
+          <span className="font-bold tracking-tight text-lg">ProductManager</span>
+        </Link>
+        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+          <nav className="flex items-center space-x-6 text-sm font-medium">
+            <Link
+              to="/products"
+              className="transition-colors hover:text-foreground/80 text-foreground"
+            >
+              Catálogo
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+}
