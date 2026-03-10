07d69f44b3aeae874d1ded5aa4ffa45a14686719
elkingutierrex <elkin.gutierrex@gmail.com>
2026-03-10 16:20:24 -0500

    style(logo)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex h-20 max-w-screen-2xl items-center px-4 md:px-8">
         <Link to="/" className="mr-6 flex items-center space-x-2">
-          <img src="public/assets/logo.png" alt="logo" className="w-15 flex border rounded-lg" />
+          <img src="public/logo.png" alt="logo" className="w-15 flex border rounded-lg" />
           <span className="font-bold tracking-tight text-lg">
              <span className="txt-logo"> egx </span> 
              | ProductManager
