fd0cb6e4bc4e437bb1f54185fd9337d50a7cded2
elkingutierrex <elkin.gutierrex@gmail.com>
2026-03-10 17:02:43 -0500

    version

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
             <span
               className="transition-colors hover:text-foreground/80 text-foreground"
             >
-              Catálogo V0.0.1
+              Catálogo V0.0.2
             </span>
           </nav>
         </div>
